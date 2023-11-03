d90a11b633cc92a11e493f6226ac07f6235fb586
sakthi <sakthiece08@gmail.com>
2023-11-03 15:46:42 -0400

    Add JwtAuthenticationConverter

--- src/main/java/com/teqmonic/keycloak/configuration/CustomSecurityConfiguration.java
+++ src/main/java/com/teqmonic/keycloak/configuration/CustomSecurityConfiguration.java
@@ -2,26 +2,26 @@ package com.teqmonic.keycloak.configuration;
 
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import com.teqmonic.keycloak.configuration.security.JwtAuthConverter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class CustomSecurityConfiguration {
 	
-	@Autowired
-	private JwtAuthConverter jwtAuthConverter;
+	//@Autowired
+	//private JwtAuthConverter jwtAuthConverter;
 
 	@Bean
 	SecurityFilterChain adminSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -34,9 +34,26 @@ public class CustomSecurityConfiguration {
 				.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.oauth2ResourceServer(
-						//server -> server.jwt(Customizer.withDefaults()))
-						server -> server.jwt(configurer -> configurer.jwtAuthenticationConverter(jwtAuthConverter)))
+						server -> server.jwt(Customizer.withDefaults()))
+						//server -> server.jwt(configurer -> configurer.jwtAuthenticationConverter(jwtAuthConverter)))
 		        .build();
+	}
+	
+	@Bean
+	JwtAuthenticationConverter jwtAuthenticationConverter() {
+		JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+		jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName("roles"); // Default "scope" or "scp"
+		jwtGrantedAuthoritiesConverter.setAuthorityPrefix(""); // Default "SCOPE_"
+		JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
+		jwtConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+		return jwtConverter;
+	}
+	
+	@Bean
+	DefaultMethodSecurityExpressionHandler defaultMethodSecurityExpressionHandler() {
+		DefaultMethodSecurityExpressionHandler handler = new DefaultMethodSecurityExpressionHandler();
+		handler.setDefaultRolePrefix(""); // Default "ROLE_"
+		return handler;
 	}
 
 }
